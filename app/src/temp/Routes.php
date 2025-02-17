3cd24552793591d6072f58d1b9855f840a7db17a
Tom Depussay <depussay.tom1@gmail.com>
2025-02-17 09:28:21 +0100

    Suppression temporaire du dossier config

---
+++ app/src/temp/Routes.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Config;
+
+class Routes {
+    public static array $routes = [
+        [
+            "path" => "/",
+            "method" => ["GET"],
+            "controller" => "HomeController",
+            "function" => "index"
+        ],
+        [
+            "path" => "/connexion",
+            "method" => ["GET", "POST"],
+            "controller" => "AuthController",
+            "function" => "login"
+        ],
+        [
+            "path" => "/inscription",
+            "method" => ["GET", "POST"],
+            "controller" => "AuthController",
+            "function" => "register"
+        ],
+        [
+            "path" => "/deconnexion",
+            "method" => ["GET"],
+            "controller" => "AuthController",
+            "function" => "logout"
+        ],
+        [
+            "path" => "/mot-de-passe-oublie",
+            "method" => ["GET", "POST"],
+            "controller" => "AuthController",
+            "function" => "forgotPassword"
+        ],
+        [
+            "path" => "/groupes",
+            "method" => ["GET"],
+            "controller" => "GroupController",
+            "function" => "index"
+        ],
+        [
+            "path" => "/groupes/creer",
+            "method" => ["GET", "POST"],
+            "controller" => "GroupController",
+            "function" => "create"
+        ],
+        [
+            "path" => "/groupes/{id_group}",
+            "method" => ["GET"],
+            "controller" => "GroupController",
+            "function" => "show"
+        ],
+        [
+            "path" => "/groupes/{id_group}/gerer",
+            "method" => ["GET", "POST"],
+            "controller" => "GroupController",
+            "function" => "edit"
+        ],
+        [
+            "path" => "/groupes/{id_group}/ajouter",
+            "method" => ["GET", "POST"],
+            "controller" => "PictureController",
+            "function" => "create"
+        ],
+        [
+            "path" => "/photos/{url_picture}",
+            "method" => ["GET", "POST"],
+            "controller" => "PictureController",
+            "function" => "show"
+        ],
+        [
+            "path" => "/photos/{url_picture}/commenter",
+            "method" => ["GET", "POST"],
+            "controller" => "PictureController",
+            "function" => "comment"
+        ],
+        [
+            "path" => "/photos/{url_picture}/modifier",
+            "method" => ["GET", "POST"],
+            "controller" => "PictureController",
+            "function" => "edit"
+        ],
+        [
+            "path" => "/photos/{url_picture}/supprimer",
+            "method" => ["POST"],
+            "controller" => "PictureController",
+            "function" => "delete"
+        ],
+        [
+            "path" => "/design",
+            "method" => ["GET"],
+            "controller" => "HomeController",
+            "function" => "design"
+        ]
+    ];
+}
