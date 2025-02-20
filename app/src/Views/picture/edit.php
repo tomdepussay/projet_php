938143b774b1fa914b495e3fd863306edc714ea0
Tom Depussay <depussay.tom1@gmail.com>
2025-02-21 00:42:27 +0100

    Fix edit picture

--- app/src/Views/picture/edit.php
+++ app/src/Views/picture/edit.php
@@ -4,10 +4,10 @@
 <h2 class="subtitle">Modifier la photo</h2>
 
 <form class="edit-photo" action="/photos/<?= $picture->getUrl() ?>/modifier" method="POST">
-    <textarea class="textarea" rows="5" cols="30" placeholder="Description" id="description" name="comment"
+    <textarea class="textarea" rows="5" cols="30" placeholder="Description" id="description" name="description"
         resize="none" required><?= $description ?? $picture->getDescription() ?></textarea>
     <div class="checkbox-label">
-        <input type="checkbox" class="checkbox" name="public" id="public">
+        <input type="checkbox" class="checkbox" name="public" id="public" <?= $picture->getPublicAccess() ? "checked" : "" ?>>
         <label class="label label--input-desc" for="public">Rendre la photo publique</label>
     </div>
     <input class="button button--primary button-left" type="submit" value="Modifier la photo" name="submit">
@@ -16,6 +16,6 @@
     <button type="submit" value="Supprimer la photo" name="submit"
         class="button button--icon button--icon-red button-left">
         <img src=" /public/medias/trash.png" class="button--icon-image" />
-        <span>Supprimer le groupe</span>
+        <span>Supprimer la photo</span>
     </button>
 </form>
